05d9838abdb67722c940a5bd9261ad064ef40e5b
Henry Ng <henryfc.ng@gmail.com>
2015-04-30 18:26:36 -0400

    beginning tokenization

--- server/config/users.js
+++ server/config/users.js
@@ -2,14 +2,19 @@ var db = require('../config/dbConfig.js');
 var Sequelize = require('sequelize');
 var bcrypt = require('bcrypt-nodejs');
 
-var User = db.define('User',
+var User = db.define('User', 
   {
+    
     username: Sequelize.STRING,
+    email: Sequelize.STRING,
     password: Sequelize.STRING
+
   },
+
   {
     instanceMethods: {
-      setPassword : function(newPassword, callback) {
+
+      encryptPassword : function(newPassword, callback) {
         var self = this;
         bcrypt.hash(newPassword, null, null, function(err, hash){
           if (!err) {
@@ -17,11 +22,13 @@ var User = db.define('User',
           }
         });
       },
-      checkPassword : function(attemptedPassword) {
+
+      authenticate: function(attemptedPassword) {
         return bcrypt.compareSync(attemptedPassword, this.get('password'));
       }
     }
   }
+
 );
 
 db.sync();
