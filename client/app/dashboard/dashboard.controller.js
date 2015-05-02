fd4800488cda05acb5b546121526024c84f32c11
Craig Smith <craigsmith24@gmail.com>
2015-05-01 20:22:57 -0700

    dashboard preliminary features complete

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -2,15 +2,30 @@
 
 angular.module('familyThiefApp')
   .controller('DashboardCtrl', function ($scope, $http, Auth) {
-    $scope.awesomeThings = [];
+
+    $scope.username = 'Craig';
     
-    $scope.getCurrentUser = Auth.getCurrentUser;
+    $scope.user = Auth.getCurrentUser();
 
-    // $scope.username = $scope.getCurrentUser().username;
-	$scope.username = 'Brandon';
-	    
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+    $scope.helpRequests = [{title:'the long road to nowhere', text:'', comments:''},
+                          {title: 'the prodigal son', text:'', comments:''},
+                          {title: 'the broken family', text:'', comments:''}];
+    
+    $scope.contributions = [{submitter:'John Doe', title:'Superiority Complex'},
+                            {submitter: 'Liz Penny', title: 'Meow Meow Meow'}];
 
+    $scope.helpFromOthers = [{submitter: 'Adam Van Antwerp', title: 'Portland or Bust'},
+                                  {submitter: 'Brandon Ellis', title: 'The Life of Yinz'}];
+
+    $scope.commentsFromOthers = ['hey, loved your piece!....','Great Work! Small suggestion....' ];
+
+
+  // $scope.username = $scope.user.username;
+
+ //  $scope.contributions = $scope.user.contributions;
+
+ //  $scope.unseenHelpFromOthers = $scope.user.helpFromOthers.length;
+
+ //  $scope.unseenCommentsFromOthers = $scope.user.commentsFromOthers.length;
+    
   });
