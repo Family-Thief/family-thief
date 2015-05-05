83ddf1d30aca86cb2a29934e557e75fb3a0fa440
Henry Ng <henryfc.ng@gmail.com>
2015-05-05 17:39:30 -0400

    Contribution Model

--- server/models/contributions.js
+++ server/models/contributions.js
@@ -1,10 +1,22 @@
 var db = require('../config/dbConfig.js');
 var Sequelize = require('sequelize');
-
+var User = require('./user.js');
+var Project = require('./project.js');
 
 var Contribution = db.define('Contribution',
   {
-    helpedUsername:
+    contributionText: Sequelize.TEXT,
+    unseenHelp: Sequelize.BOOLEAN,
+    contributor: {
+      type: Sequelize.INTEGER,
+      references: User,
+      referencesKey: "id"
+    },
+    project: {
+      type: Sequelize.INTEGER,
+      references: Project,
+      referencesKey: "id"
+    }
 
   }
 );
