dbb5de2e84a78f87d1e87be6b7be9b517638bcb3
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-05-06 17:49:35 -0400

    Building the HelpRequest service

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('familyThiefApp')
-  .controller('DashboardCtrl', function ($scope, $http, Auth) {
+  .controller('DashboardCtrl', function ($scope, $http, Auth, HelpRequest) {
     
     // inject data of currently logged-in user into this controller
     $scope.user = Auth.getCurrentUser();
-
+    
+    $scope.searchResults = [{id: 1, title: "A novel", summary: "just help me", origDate: "May 1st 2015"}];
     //search for a project containing the query strings
     $scope.search = function() {
       $http({
@@ -13,14 +14,13 @@ angular.module('familyThiefApp')
         method: "GET",
         params: {search: $scope.searchString}
       }).success(function(data, status) {
-        console.log(data);
-        console.log("hey");
         $scope.searchResults = resultArray;
       });
     };
     
     $scope.loadHelpRequest = function(id) {
-
+      HelpRequest.setHelpRequestId(id);
+      console.log(HelpRequest.getCurrentHelpRequest());
     }
     
   });
--- client/components/auth/helpRequest.service.js
+++ client/components/auth/helpRequest.service.js
@@ -4,9 +4,17 @@
 
 angular.module('familyThiefApp')
   .factory('HelpRequest', function ($resource) {
+    var currentHelpRequest;
     return $resource('/api/helpRequests', {
+      id: '@_id'
     },
     {
-      
+      setHelpRequestId: function(id) {
+        console.log("inside");
+        currentHelpRequest = id;
+      },
+      getCurrentHelpRequest: function() {
+        return currentHelpRequest;
+      }
     });
   });
