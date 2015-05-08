7a9f1aeec99ad267496032710c255675eb95f219
Craig Smith <craigsmith24@gmail.com>
2015-05-07 18:30:09 -0700

    created view for page with 25 most reecent help requests

---
+++ client/app/allHelpRequest/allHelpRequest.controller.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('familyThiefApp')
+  .controller('AllHelpRequestCtrl', function ($scope, $http, Auth, HelpRequest, $location) {
+    
+    // inject data of currently logged-in user into this controller
+    $scope.user = Auth.getCurrentUser();
+    
+    $scope.searchResults = [{id: 1, title: "A novel", summary: "just help me", origDate: "May 1st 2015"}];
+    //search for a project containing the query strings
+    $scope.search = function() {
+      $http({
+        url: "api/helpRequests",
+        method: "GET",
+        params: {search: $scope.searchString}
+      }).success(function(data, status) {
+        $scope.searchResults = resultArray;
+      });
+    };
+    
+    $scope.loadHelpRequest = function(id) {
+      console.log(id);
+      Auth.setHelpRequest(id);
+      $location.path('/help-request');
+    };
+    
+  });
